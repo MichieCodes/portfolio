9eaa785c8c0ca59f9619a319197f700084a29ecb
MichieTuts <michietuts@gmail.com>
2022-01-25 21:36:24 -0600

    Reformat skills serverless function

--- serverless-functions/routes/skills.js
+++ serverless-functions/routes/skills.js
@@ -1,7 +1,9 @@
 const { getAll } = require("../airtable");
 
 exports.handler = async function(e) {
-  if(e.httpMethod === 'GET') {
+  if(e.httpMethod !== 'GET') return {statusCode: 405, body: JSON.stringify([])};
+
+  try {
     return {
       statusCode: 200,
       body: JSON.stringify(await getAll('Skills')),
@@ -10,7 +12,7 @@ exports.handler = async function(e) {
         'cache-control': 's-maxage=120, immutable'
       }
     };
+  } catch (error) {
+    return {statusCode: 500, body: JSON.stringify([])};
   }
-
-  return {statusCode: 405, body: JSON.stringify([])};
 }
